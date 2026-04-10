5d3859d9b087bd96199d263a6b7a3753029dde11
Jan <306341246@qq.com>
2026-04-11 01:41:09 +0800

    fix bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   site: "https://keyan0412.github.io",
   base: "/my-blog",
+  trailingSlash: "always",
   vite: {
     plugins: [tailwindcss()],
   },
